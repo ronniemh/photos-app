2060aa6d18e520110de336bf5251aa49a6d21665
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-11-20 11:54:16 -0500

    add eureka client properties

--- photosAppApiUsers/src/main/java/dev/ronniemoncayo/photoapp/api/users/PhotosAppApiUsers/PhotosAppApiUsersApplication.java
+++ photosAppApiUsers/src/main/java/dev/ronniemoncayo/photoapp/api/users/PhotosAppApiUsers/PhotosAppApiUsersApplication.java
@@ -2,8 +2,10 @@ package dev.ronniemoncayo.photoapp.api.users.PhotosAppApiUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotosAppApiUsersApplication {
 
 	public static void main(String[] args) {
