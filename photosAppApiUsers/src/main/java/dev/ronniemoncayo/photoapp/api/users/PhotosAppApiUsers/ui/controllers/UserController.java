6c26d0cc96befc2f1c24bc9769fe8cdd578f2320
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-11-20 15:26:00 -0500

    configure eureka, zuul and microservices

--- photosAppApiUsers/src/main/java/dev/ronniemoncayo/photoapp/api/users/PhotosAppApiUsers/ui/controllers/UserController.java
+++ photosAppApiUsers/src/main/java/dev/ronniemoncayo/photoapp/api/users/PhotosAppApiUsers/ui/controllers/UserController.java
@@ -1,5 +1,7 @@
 package dev.ronniemoncayo.photoapp.api.users.PhotosAppApiUsers.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
     public String status(){
-        return "working!";
+        return "working on port: "+env.getProperty("local.server.port");
     }
     
 }
