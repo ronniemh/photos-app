22656392c7034cb7248a18cbe378f8073a9055b3
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-11-20 14:29:01 -0500

    add zuul gateway

---
+++ PhotosAppApiZuulGateway/src/main/java/dev/ronniemoncayo/photoapp/api/gateway/PhotosAppApiZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.ronniemoncayo.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosAppApiZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosAppApiZuulGatewayApplication.class, args);
+	}
+
+}
